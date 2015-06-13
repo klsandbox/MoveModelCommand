938b9576e1fd7d1b6b6d412d485939ed1a2ca62b
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-06-13 08:29:56 +0800

    use service provider

--- src/Klsandbox/MoveModelCommand/MoveModelCommandServiceProvider.php
+++ src/Klsandbox/MoveModelCommand/MoveModelCommandServiceProvider.php
@@ -11,24 +11,34 @@ class MoveModelCommandServiceProvider extends ServiceProvider {
 	 */
 	protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+	
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app->singleton('command.klsandbox.editmovemodel', function($app) {
+            return new EditMoveModel();
+        });
+        $this->app->singleton('command.klsandbox.listmodel', function($app) {
+            return new ListModel();
+        });
+        
+        $this->commands('command.klsandbox.editmovemodel');
+        $this->commands('command.klsandbox.listmodel');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [
+            'command.klsandbox.editmovemodel',
+            'command.klsandbox.listmodel',
+        ];
+    }
 
 }
